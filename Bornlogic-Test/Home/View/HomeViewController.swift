6578ac04dfcb151b629e88a072723be53195ccc0
Erick Martins <erickmxav@gmail.com>
2024-05-12 12:14:13 -0300

    sets for previous date

--- Bornlogic-Test/Home/View/HomeViewController.swift
+++ Bornlogic-Test/Home/View/HomeViewController.swift
@@ -64,7 +64,7 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! HomeTableViewCell
         
         if let item = presenter?.item(at: indexPath.row) {
-            cell.configure(title: item.title, content: item.description, author: item.author ?? "Unknown", imageUrl: item.urlToImage ?? "")
+            cell.configure(title: item.title ?? "", content: item.description ?? "", author: item.author ?? "Unknown", imageUrl: item.urlToImage ?? "")
         }
         
         return cell
