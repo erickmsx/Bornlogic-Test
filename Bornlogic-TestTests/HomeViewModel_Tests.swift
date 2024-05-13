bf3f0837bbe03c7c6ef42962a1cc021903b3895e
Erick Martins <erickmxav@gmail.com>
2024-05-13 13:16:31 -0300

    test for items on model

--- Bornlogic-TestTests/HomeViewModel_Tests.swift
+++ Bornlogic-TestTests/HomeViewModel_Tests.swift
@@ -26,6 +26,11 @@ class HomeViewModel_Tests: XCTestCase {
         presenter.articlesFetchedMock()
         
         XCTAssertNotNil(presenter.homeModel)
-        XCTAssertEqual(presenter.homeModel?.articles[0].author, "authorTest", "Title should match")
+        XCTAssertNotNil(presenter.homeModel?.articles)
+        XCTAssertEqual(presenter.homeModel?.articles[0].author, "authorTest", "Author should match")
+        XCTAssertEqual(presenter.homeModel?.articles[0].title, "titleTest", "Title should match")
+        XCTAssertEqual(presenter.homeModel?.articles[0].urlToImage, "urlTest", "UrlToImage should match")
+        XCTAssertEqual(presenter.homeModel?.articles[0].description, "descriptionTest", "Description should match")
+        XCTAssertEqual(presenter.homeModel?.articles[0].content, "contentTest", "Content should match")
     }
 }
